0f5f48b72459abbca0cc94087d41716ad00b0892
iseruuuuu <ryutaro.iseki23@gmail.com>
2022-07-02 15:16:05 +0900

    ウォレットの新規作成

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
 import React from "react";
-import HeadComponent from '../components/Head';
+import HeadComponent from "../components/Head";
+import { useState } from "react";
+import { Keypair } from "@solana/web3.js";
+import * as Bip39 from "bip39";
 
 export default function Home() {
+  const [mnemonic, setMnemonic] = useState(null);
+  const [account, setAccount] = useState(null);
+
+  const generateWallet = () => {
+    //ユーザーがメモして安全に保管できるように、ニーモニックフレーズを設定し、表示することができる
+    const generatedMnemonic = Bip39.generateMnemonic();
+    setMnemonic(generatedMnemonic);
+    console.log("seed", generatedMnemonic);
+
+    const seed = Bip39.mnemonicToSeedSync(generatedMnemonic).slice(0, 32);
+    console.log("seed", seed);
+
+    const newAccount = Keypair.fromSeed(seed);
+    console.log("new Account", newAccount.publicKey.toString());
+
+    setAccount(newAccount);
+  };
+
   return (
     <div>
-      <HeadComponent/>
+      <HeadComponent />
       <div className="p-10">
         <h1 className="text-5xl font-extrabold tracking-tight text-gray-900">
           <span className="text-[#9945FF]">Solana</span>ウォレットを作ろう！
@@ -16,39 +37,76 @@ export default function Home() {
         <hr className="my-6" />
 
         <div>
-          <h3 className="p-2 border-dotted border-l-8 border-l-indigo-600">My Wallet</h3>
+          <h3 className="p-2 border-dotted border-l-8 border-l-indigo-600">
+            My Wallet
+          </h3>
+
+          {account && (
+            <div className="my-6 text-indigo-600 font-bold">
+              アドレス: {account.publicKey.toString()}
+            </div>
+          )}
         </div>
 
         <hr className="my-6" />
 
         <div>
-          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP1: ウォレットを新規作成する</h2>
+          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">
+            STEP1: ウォレットを新規作成する
+          </h2>
+
+          <button
+            className="p-2 my-6 text-white bg-indigo-500 focus:ring focus:ring-indigo-300 rounded-lg cursor-pointer"
+            onClick={generateWallet}
+          >
+            ウォレットを生成
+          </button>
+          {mnemonic && (
+            <>
+              <div className="mt-1 p-4 border border-gray-300 bg-gray-200">
+                {mnemonic}
+              </div>
+              <strong className="text-xs">
+                このフレーズは秘密にして、安全に保管してください。このフレーズが漏洩すると、誰でもあなたの資産にアクセスできてしまいます。
+                <br />
+                オンライン銀行口座のパスワードのようなものだと考えてください。
+              </strong>
+            </>
+          )}
         </div>
 
         <hr className="my-6" />
 
         <div>
-          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP2: 既存のウォレットをインポートする</h2>
+          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">
+            STEP2: 既存のウォレットをインポートする
+          </h2>
         </div>
 
         <hr className="my-6" />
 
         <div>
-          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP3: 残高を取得する</h2>
+          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">
+            STEP3: 残高を取得する
+          </h2>
         </div>
 
         <hr className="my-6" />
 
         <div>
-          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP4: エアドロップ機能を実装する</h2>
+          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">
+            STEP4: エアドロップ機能を実装する
+          </h2>
         </div>
 
         <hr className="my-6" />
 
         <div>
-          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP5: 送金機能を実装する</h2>
+          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">
+            STEP5: 送金機能を実装する
+          </h2>
         </div>
       </div>
     </div>
-  )
+  );
 }
